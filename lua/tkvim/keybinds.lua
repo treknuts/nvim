b195046891c9dbe709a0194dff911678e17d42ab
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2025-07-23 09:30:06 -0500

    Add floating terminal. I need to figure out how to actually kill buffers.

--- lua/tkvim/keybinds.lua
+++ lua/tkvim/keybinds.lua
@@ -5,6 +5,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- refactor whatever your cursor is on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>st", "<cmd>Floaterminal<CR>")
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
 -- Vim motions between windows
 vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>")
@@ -16,8 +18,7 @@ vim.keymap.set("n", "<Tab>", "<cmd>BufferNext<CR>")
 vim.keymap.set("n", "<S-Tab>", "<cmd>BufferPrevious<CR>")
 -- Quit Buffer
 vim.keymap.set("n", "<leader>qb", "<cmd>BufferClose<CR>")
--- Get out of Terminal Mode
-vim.keymap.set("t", "<C-w>n", "<C-\\><C-n><C-w>h", { silent = true })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlights text when yanking",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
@@ -25,18 +26,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
-
-vim.api.nvim_create_autocmd("TermOpen", {
-  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
-})
-
-vim.keymap.set("n", "<leader>st", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
-end)
