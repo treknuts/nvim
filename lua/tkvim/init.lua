1c649e8da07883bf343b9760f7fbf5bc494a96cc
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2025-08-01 07:37:25 -0500

    Try gruvbox

--- lua/tkvim/init.lua
+++ lua/tkvim/init.lua
@@ -6,8 +6,6 @@ require("tkvim.lazy_init")
 require("tkvim.keybinds")
 require("tkvim.set")
 
--- Customize theme
-require("catppuccin").setup({ flavour = "mocha" })
-
 -- setup must be called before loading
-vim.cmd.colorscheme("catppuccin")
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
--- lua/tkvim/plugins/theme.lua
+++ lua/tkvim/plugins/theme.lua
@@ -1,16 +1,6 @@
-return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-}
+return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true }
 --   {
---   "navarasu/onedark.nvim",
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     require("onedark").setup({
---       style = "darker",
---     })
---     -- Enable theme
---     require("onedark").load()
---   end,
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
 -- }
